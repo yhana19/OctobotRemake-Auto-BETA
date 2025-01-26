a99914d9a7a441d15c9f25ffe239e9637c4f0d94
Yhana alvarez <yasisarmia@gmail.com>
2025-01-27 00:48:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-/*
-(⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠) DEV: REJARD BENTAZAR 
-
-* CREDITS TO: 
-** AIZENTEL 
-**NTKANG
-** FCA-UNOFFICIAL
-*/
-
-const chalk =  require('chalk');
 const { spawn } = require("child_process");
 const path = require('path');
-const YAFB_SYSTEM = "auto.js";
 
-const YAFB_CORE = path.join(__dirname, YAFB_SYSTEM);
-console.log(chalk.green('[SUCCES] - OctobotRemake is Now Ready to Use'));
+const SCRIPT_FILE = "auto.js";
+const SCRIPT_PATH = path.join(__dirname, SCRIPT_FILE);
+
+
 function start() {
-    const main = spawn("node", [YAFB_SYSTEM], {
+    const main = spawn("node", [SCRIPT_PATH], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
     });
+
     main.on("close", (exitCode) => {
         if (exitCode === 0) {
-            console.log(chalk.red('YAFB MAIN SYSTEM CLOSE!'));
+            console.log("Main process exited with code 0");
         } else if (exitCode === 1) {
-            console.log(chalk.red('YAFB MAIN SYSTEM RESTARTING PLS WAIT!'));
+            console.log("Main process exited with code 1. Restarting...");
             start();
         }  else {
             console.error(`Main process exited with code ${exitCode}`);
         }
     });
 }
+
 start();
